bedb97f95df885399d9a6dea2856fc5817a0f5ec
Bjorn-Strom <bjorn.ivar.strom@bekk.no>
2021-03-30 11:55:57 +0200

    Rename program and make it a library

---
+++ Fet.fs
@@ -0,0 +1,34 @@
+namespace Fet
+
+    [<RequireQualifiedAccess>]
+    module expect =
+        let equal (actual: 'a) (expected: 'a): unit =
+            if actual <> expected then
+                failwith $"expected {actual} to be equal {expected}."
+
+    module test =
+        type TestList = { Name: string; Tests: (string * (unit -> unit)) list }
+        let test (name: string) (f: unit -> unit) = name, f
+        let testList (name: string) testList: TestList = { Name = name; Tests = testList}
+
+        let runAllTests (tests: TestList list) =
+            let stats =
+                tests
+                |> List.map (fun testList ->
+                    printfn "Test suite: %A" testList.Name
+                    testList.Tests
+                    |> Seq.fold (fun stats (k, v) ->
+                        try
+                            v ()
+                            printfn $"\t\u2705  - {k}"
+                            {| Succeeded = stats.Succeeded + 1; Failed = stats.Failed |}
+                        with
+                        | e ->
+                            printfn $"\t\u274c  - Error in test \"{k}\" failed with: {e.Message}"
+                            {| Succeeded = stats.Succeeded; Failed = stats.Failed + 1 |} ) {| Succeeded = 0; Failed = 0 |}
+                    )
+                |> List.reduce (fun acc x -> {| Succeeded = acc.Succeeded + x.Succeeded; Failed = acc.Failed + x.Failed |})
+
+            printfn $"\nRan {stats.Succeeded + stats.Failed}"
+            printfn $"Succeeded: {stats.Succeeded}"
+            printfn $"Failed: {stats.Failed}"
