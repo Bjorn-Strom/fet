7a12b06d2c86de4db5a6096b6b6a5c7d2f16db30
unknown <bjornivar.strom@gmail.com>
2023-02-13 23:28:27 +0100

    nicer output

--- fet/Fet.fs
+++ fet/Fet.fs
@@ -4,34 +4,67 @@ namespace Fet
     module Expect =
         let equal (actual: 'a) (expected: 'a): unit =
             if actual <> expected then
-                failwith $"\n\texpected: \n\t\t{actual} \n\tto be equal: \n\t\t{expected}"
+                failwith $"Expected: {expected}\nActual: {actual}"
 
     [<AutoOpen>]
     module Test =
+        type Test = {
+            Name: string
+            Message: string option
+        }
+        type TestListResult = {
+            ListName: string
+            Succeeded: int
+            Tests: Test list
+        }
         type TestList = { Name: string; Tests: (string * (unit -> unit)) list }
         let test (name: string) (f: unit -> unit) = name, f
         let testList (name: string) testList: TestList = { Name = name; Tests = testList}
 
         let runTests (tests: TestList list) =
-            let stats =
+            let testResults =
                 tests
                 |> List.map (fun testList ->
-                    printfn "Test suite: %A" testList.Name
-                    testList.Tests
-                    |> Seq.fold (fun stats (k, v) ->
+                    //printfn "Test suite: %A" testList.Name
+                    testList.Name, testList.Tests
+                    |> Seq.map (fun (key, func)  ->
                         try
-                            v ()
-                            printfn $"\t\u2705  - {k}"
-                            {| Succeeded = stats.Succeeded + 1; Failed = stats.Failed |}
+                            func ()
+                            //printfn $"\t\u2705  - {k}"
+                            { Name = key; Message = None }
                         with
                         | e ->
-                            printfn $"\t\u274c  - Error in test \"{k}\" failed with: {e.Message}"
-                            {| Succeeded = stats.Succeeded; Failed = stats.Failed + 1 |} ) {| Succeeded = 0; Failed = 0 |}
-                    )
-                |> List.reduce (fun acc x -> {| Succeeded = acc.Succeeded + x.Succeeded; Failed = acc.Failed + x.Failed |})
+                            //printfn $"\t\u274c  - Error in test \"{k}\" failed with: {e.Message}"
+                            { Name = key; Message = Some e.Message}
+                    ))
 
-            printfn $"\nRan: {stats.Succeeded + stats.Failed}"
-            printfn $"Succeeded: {stats.Succeeded}"
-            printfn $"Failed: {stats.Failed}"
-            if stats.Failed > 0 then
-                failwith $"Tests failed with {stats.Failed} errors."
+            testResults
+            |> List.iter (fun (testName, stats) -> 
+                printfn $"Test list: {testName}"
+                printfn "------------------------------"
+                stats
+                |> Seq.iter (fun (failed: Test) ->
+                    if failed.Message.IsSome then 
+                        printfn $"[FAIL] - {failed.Name}"
+                        printfn "%A" failed.Message.Value)
+                let total = Seq.length stats
+                let succeeded =
+                    stats
+                    |> Seq.filter (fun (x: Test) -> x.Message.IsNone)
+                    |> Seq.length
+                let failed = total-succeeded;
+                printfn $"Tests run: {total}, Passed: {succeeded}, Failed: {failed}"
+                printfn "")
+
+            let total = Seq.length testResults
+            let succeeded =
+                testResults
+                |> Seq.map snd
+                |> Seq.filter (fun (x: Test) -> x.Message.IsNone)
+                |> Seq.length
+
+            printfn "Total tests run: {}, Passed: {}, Failed: {}"
+
+// TODO: Total count
+// TODO: Send verbose in command line to see all
+// TODO: XML thing?
